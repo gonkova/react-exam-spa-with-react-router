705da53ec98eb93f969b7c84d001c6d2a09cf00d
Boryana Gonkova <gonkova@mail.bg>
2023-11-24 16:54:50 +0200

    Премахване на излишни празни редове

--- src/layout/WithMainLayout.js
+++ src/layout/WithMainLayout.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Header from '../components/header/Header';
 
-
-
-
 export default function WithMainLayout(Component) {
     function layout(props) {
         return (
             <>
-               <Header />
+                <Header />
                 <Component {...props} />
             </>
         );
